60b497375aa093b0e8acc5b4efa3dac6691e9721
Brendan Asselstine <mail.asselstine@gmail.com>
2020-12-07 15:42:12 -0800

    Published 1.0.0 and integrated OZ dependency for simplicity

--- contracts/FixedPoint.sol
+++ contracts/FixedPoint.sol
@@ -18,7 +18,7 @@ along with PoolTogether.  If not, see <https://www.gnu.org/licenses/>.
 
 pragma solidity 0.6.4;
 
-import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
+import "./external/openzeppelin/SafeMath.sol";
 
 /**
  * @author Brendan Asselstine
